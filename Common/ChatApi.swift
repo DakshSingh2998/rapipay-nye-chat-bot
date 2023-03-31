663b419e2a3f746e5a886fcf2c5f27bdc5a9426f
Daksh <daksh2998@gmail.com>
2023-03-31 13:49:12 +0530

    31mar 1.49pm

--- Common/ChatApi.swift
+++ Common/ChatApi.swift
@@ -50,7 +50,7 @@ class ChatApi{
         let httpMethod = "POST"
         let addValue = ["Project-ID" : Common.shared.projectId, "User-Name" : userName, "User-Secret" : pass]
         let setValue = ["Content-Type" : "application/json", "Accept" : "application/json"]
-        NetworkManager.shared.connect(parameters: parameters, url: url, httpMethod: httpMethod, setValue: setValue, addValue: addValue, completition: {data, error in
+        NetworkManager.shared.connect(parameters: parameters, url: url, httpMethod: httpMethod, setValue: setValue, addValue: addValue, timeOutInterval: Double.infinity, completition: {data, error in
             completition?(data, error)
             
         })
